21467f8818ca8313dbdb0203023f440fff5068e3
Marcel Beck <beck@vital-services.de>
2026-06-29 12:27:10 +0200

    fix: hide validation warnings until submit or field interaction

--- src/extensions/components/registration/FormAccordion/FormAccordionItem.tsx
+++ src/extensions/components/registration/FormAccordion/FormAccordionItem.tsx
@@ -45,9 +45,12 @@ export const FormAccordionItem: FC<
 		.getFieldsError(formFields)
 		.some((error) => error.errors.length !== 0);
 
-	const isValid = !(
-		(submitted || formContext.isFieldsTouched(fieldsToCheck)) && isFieldsInValid
+	const isTouched = formContext.isFieldsTouched(
+		fieldsToCheck.filter((f) => f !== 'topicIds[]'),
+		false
 	);
+
+	const isValid = !((submitted || isTouched) && isFieldsInValid);
 
 	const { t: translate } = useTranslation();
 
