539386386cc65e329b3a632d6a72891d09878bbe
Marcel Beck <beck@vital-services.de>
2026-06-29 12:01:01 +0200

    fix: use digisucht theme as default

--- src/initApp.tsx
+++ src/initApp.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './components/app/app';
-import { Stage } from './components/stage/stage';
-import { config } from './resources/scripts/config';
+import { Stage } from './extensions/components/stage/stage';
+import { config, routePathNames } from './extensions/resources/scripts/config';
+import { Imprint } from './extensions/components/legalInformationLinks/Imprint';
+import { Privacy } from './extensions/components/legalInformationLinks/Privacy';
+import { TermsAndConditions } from './extensions/components/legalInformationLinks/TermsAndConditions';
 
 ReactDOM.render(
-	<App config={config} stageComponent={Stage} />,
+	<App
+		extraRoutes={[
+			{
+				route: { path: routePathNames.termsAndConditions },
+				component: TermsAndConditions
+			},
+			{ route: { path: routePathNames.imprint }, component: Imprint },
+			{ route: { path: routePathNames.privacy }, component: Privacy }
+		]}
+		stageComponent={Stage}
+		legalLinks={[
+			{
+				url: routePathNames.imprint,
+				label: 'login.legal.infoText.impressum'
+			},
+			{
+				url: routePathNames.privacy,
+				label: 'login.legal.infoText.dataprotection',
+				registration: true
+			}
+		]}
+		config={config}
+	/>,
 	document.getElementById('appRoot')
 );
